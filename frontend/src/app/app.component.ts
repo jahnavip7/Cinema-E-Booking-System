9687d4448f0559684425474537fee4bf735f4561
Akil <akilmir8222003@gmail.com>
2024-09-20 15:39:17 -0400

    Bunch of other pages (No functionality)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { PaymentInformationComponent } from './payment-information/payment-information.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { AdminPortalComponent } from './admin-portal/admin-portal.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { EditMovieComponent } from './edit-movie/edit-movie.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, RegistrationComponent, PaymentInformationComponent, EditProfileComponent],
+  imports: [RouterOutlet, LoginComponent, RegistrationComponent, PaymentInformationComponent, EditProfileComponent, 
+    OrderDetailsComponent, AdminPortalComponent, AddMovieComponent, EditMovieComponent, CheckoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
