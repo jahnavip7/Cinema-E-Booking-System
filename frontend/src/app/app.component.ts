a02dabf0db6c394be1bba967f705cdb6ee071ea4
Akil <akilmir8222003@gmail.com>
2024-09-21 17:17:27 -0400

    styling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,12 +9,17 @@ import { AdminPortalComponent } from './admin-portal/admin-portal.component';
 import { AddMovieComponent } from './add-movie/add-movie.component';
 import { EditMovieComponent } from './edit-movie/edit-movie.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { VerifyAccountComponent } from './verify-account/verify-account.component';
+import { ManagePromotionsComponent } from './manage-promotions/manage-promotions.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LoginComponent, RegistrationComponent, PaymentInformationComponent, EditProfileComponent, 
-    OrderDetailsComponent, AdminPortalComponent, AddMovieComponent, EditMovieComponent, CheckoutComponent],
+    OrderDetailsComponent, AdminPortalComponent, AddMovieComponent, EditMovieComponent, CheckoutComponent, ChangePasswordComponent, 
+    VerifyAccountComponent, ManagePromotionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
