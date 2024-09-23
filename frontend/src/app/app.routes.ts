bf4abe11e73d15fd0b5c0f89d5f8e5168ba3228f
Akil <akilmir8222003@gmail.com>
2024-09-22 20:17:24 -0400

    Akshita's pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SelectShowtimeComponent } from './select-showtime/select-showtime.component';
+import { SelectSeatComponent } from './select-seat/select-seat.component';
+import { OrderSummaryComponent } from './ordersummary/ordersummary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'select-showtime', component: SelectShowtimeComponent },
+  { path: 'select-seat', component: SelectSeatComponent },
+  { path: 'order-summary', component: OrderSummaryComponent },
+  { path: '', redirectTo: '/select-showtime', pathMatch: 'full' },  // Default route
+];
